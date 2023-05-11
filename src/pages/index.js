e50b7000a189ac49a3adc48a9d9bb1689a1eed4a
Sajid Jaber <sajidjaber@yahoo.com>
2023-05-11 12:39:31 -0500

    hero image and text ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
-import profilePic from "../../public/images/sajidjaber.png";
+import profilePic from "../../public/images/sajid.png";
 import Image from "next/image";
+import AnimatedText from "../components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
 export default function Home() {
   return (
     <>
@@ -11,13 +14,21 @@ export default function Home() {
             <div className="w-1/2">
               <Image src={profilePic} alt="Sajid" className="w-full h-auto" />
             </div>
-            <div className="w-1/2">
-              <h1>Turning Vision Into Reality With Code And Design</h1>
-              <p>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
-Explore my latest projects and articles, showcasing my expertise in React.js and web development</p>
-
-            </div>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText text="Versatile Full Stack MERN Developer" />
+              <p className="my-4 text-base font-medium">An adept Full Stack MERN Developer, I bring a wealth of experience in building scalable, high-performing, and robust web applications using MongoDB, Express.js, React.js, and Node.js. With a strong understanding of JavaScript and its latest ES6+ features, I deliver both front-end and back-end solutions that meet and exceed client expectations.</p>
+           
+            <div className="flex items-center self-start mt-2">
+            <Link className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-transparent hover:border-dark" href={"./resume.pdf"} download={true} target="_blank">Resume
+            <LinkArrow className={" w-6 h-6 ml-1"} />
+            
+            
+            
+            </Link>
+            <Link href={"mailto:sajidjaber@yahoo.com"} className="ml-4 text-lg font-medium capitalize text-dark underline">Contact</Link>
           </div>
+         </div> 
+             </div>
         </Layout>
       </main>
     </>
