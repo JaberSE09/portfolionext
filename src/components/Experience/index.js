b9e2149d3f427525d8900a5acd7ce51c9d36736c
Sajid Jaber <sajidjaber@yahoo.com>
2023-05-11 20:45:13 -0500

    setup the experience

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,25 +1,71 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import { useScroll, motion } from "framer-motion";
+import LiIcon from "../LiIcon";
 const Details = ({ position, company, time, address, work }) => {
   return (
-    <li>
+    <li className="my-8 first-mt-0 last-mt-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+      {" "}
+      <LiIcon />
       <div>
-        <h3>{position} &nbsp; @ {company}</h3>
-       <span>{time} | {address} </span> 
-       <p>{work}</p>
+        <h3 className="capitalize font-bold text-2xl">
+          {position} &nbsp; @ {company}
+        </h3>
+        <span className="capitalize font-medium text-dark/75">
+          {time} | {address}{" "}
+        </span>
+        <p className="font-medium w-full">{work}</p>
       </div>
     </li>
   );
 };
 
 const Experience = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
   return (
     <div className="my-64">
       <h2 className="font-bold text-8xl mb-32 w-full text-center">
         Experience
       </h2>
 
-      <div className="w-[75%] mx-auto relative"></div>
+      <div className="w-[75%] mx-auto relative" ref={ref}>
+        <motion.div className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top" />
+        <ul
+          className="w-full flex flex-col items-start justify-between"
+          style={{ scaleY: scrollYProgress }}
+        >
+          <Details
+            position={"Remote Web Developer (Freelancer)"}
+            company={"self-employed"}
+            time={"2018-Present"}
+            address={"Remote"}
+            work={
+              "As a Freelance Remote Web Developer, I use my extensive skills to create dynamic, user-friendly, and aesthetically pleasing websites. I work independently, efficiently, and remotely, collaborating with various teams to meet and surpass project expectations."
+            }
+          />
+          <Details
+            position={"Remote Web Developer (Freelancer)"}
+            company={"self-employed"}
+            time={"2018-Present"}
+            address={"Remote"}
+            work={
+              "As a Freelance Remote Web Developer, I use my extensive skills to create dynamic, user-friendly, and aesthetically pleasing websites. I work independently, efficiently, and remotely, collaborating with various teams to meet and surpass project expectations."
+            }
+          />
+          <Details
+            position={"Remote Web Developer (Freelancer)"}
+            company={"self-employed"}
+            time={"2018-Present"}
+            address={"Remote"}
+            work={
+              "As a Freelance Remote Web Developer, I use my extensive skills to create dynamic, user-friendly, and aesthetically pleasing websites. I work independently, efficiently, and remotely, collaborating with various teams to meet and surpass project expectations."
+            }
+          />
+        </ul>
+      </div>
     </div>
   );
 };
